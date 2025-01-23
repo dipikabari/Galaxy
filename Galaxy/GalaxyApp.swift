5c37f15c2f0f9c2ddadcef24e40370862608cb82
dipikabari <dipikab@y7mail.com>
2025-01-23 19:15:34 +0000

    Initial commit

---
+++ Galaxy/GalaxyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalaxyApp.swift
+//  Galaxy
+//
+//  Created by Dipika Bari on 23/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct GalaxyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
