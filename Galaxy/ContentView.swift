1370ac1ba39723857b911db5b57b2398847eb3e3
dipikabari <dipikab@y7mail.com>
2025-01-23 21:17:22 +0000

    Add function to display sun in the center of the screen

--- Galaxy/ContentView.swift
+++ Galaxy/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var earthProgress: CGFloat = 0.0
+    @State private var earthRotation: CGFloat = 0.0
+    @State private var moonProgress: CGFloat = 0.0
+    @State private var sunScale: CGFloat = 1.0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        GeometryReader { proxy in
+            let size = proxy.size
+            sun(
+                position: size.center,
+                size: 70.0
+            )
+            .shadow(
+                color: .yellow,
+                radius: sunScale * 10.0
+            )
+            
         }
-        .padding()
+    }
+    
+    private func sun(position: CGPoint, size: CGFloat) -> some View {
+        Text("🌞")
+            .font(.system(size: size))
+            .position(position)
+            .scaleEffect(
+                CGSize(
+                    width: sunScale,
+                    height: sunScale
+                )
+            )
+    }
+    
+}
+
+//MARK: - CGSize+Extensions
+extension CGSize {
+    var center: CGPoint {
+        return CGPoint(x: width / 2.0, y: height / 2.0)
     }
 }
 
